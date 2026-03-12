db9f04957d52e7bd83557e527690fe5326eb4099
a22iliakiaki <a22iliakiaki@localhost.localdomain>
2026-03-12 14:00:46 +0100

    Prueba php

--- registrar.php
+++ registrar.php
@@ -1,4 +1,3 @@
-<?php include_once "header.php"; ?>
 
 <?php
 $mysqli = include_once "conexion.php";
@@ -11,20 +10,4 @@ VALUES
 $sentencia->bind_param("ss", $nombre, $descripcion);
 $sentencia->execute();
 header("Location: mostrar.php");?>
-<div class="row">
-    <div class="col-12">
-        <h1>Registrar videojuego</h1>
-        <form action="registrar.php" method="POST">
-            <div class="form-group">
-                <label for="nombre">Nombre</label>
-                <input placeholder="Nombre" class="form-control" type="text" name="nombre" id="nombre" required>
-            </div>
-            <div class="form-group">
-                <label for="descripcion">Descripción</label>
-                <textarea placeholder="Descripción" class="form-control" name="descripcion" id="descripcion" cols="30" rows="10" required></textarea>
-            </div>
-            <div class="form-group"><button class="btn btn-success">Guardar</button></div>
-        </form>
-    </div>
-</div>
-<?php include_once "footer.php"; ?>
+
