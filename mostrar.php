72d409cf7c3db0032b440bd86fd7088308c9a023
a22iliakiaki <a22iliakiaki@hINFO10-B5.xiam.cat>
2026-03-26 14:10:46 +0100

    add colors to different tipos

--- mostrar.php
+++ mostrar.php
@@ -21,7 +21,27 @@ $videojuegos = $resultado->fetch_all(MYSQLI_ASSOC);?>
                         <td><?php echo $videojuego["id"] ?></td>
                         <td><?php echo $videojuego["nombre"] ?></td>
                         <td><?php echo $videojuego["descripcion"] ?></td>
-                        <td><?php echo $videojuego["tipo"] ?></td>
+                        <td>
+                            <?php 
+                                $color = "black";
+                                $tipo_valor = $videojuego["tipo"];
+
+                                switch ($tipo_valor) {
+                                    case 'role':
+                                        $color = "blue";
+                                        break;
+                                    case 'sport':
+                                        $color = "yellow";
+                                        break;
+                                    case 'action': 
+                                        $color = "green";
+                                        break;
+                                }
+                                ?>
+                                <p style="color: <?php echo $color; ?>; font-weight: bold;">
+                                    <?php echo $tipo_valor; ?>
+                                </p>
+                        </td>
                         <td>
                             <a href="update.php?id=<?php echo $videojuego["id"] ?>">Editar</a>
                         </td>
